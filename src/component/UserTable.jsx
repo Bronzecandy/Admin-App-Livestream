335d7ec787c8bc26c6a7025464f0437038641044
Tran Nam <namtran09022004@gmail.com>
2024-10-25 21:54:04 +0700

    adjust users

--- src/component/UserTable.jsx
+++ src/component/UserTable.jsx
@@ -67,15 +67,27 @@ const UserTable = () => {
                         <input type="text" placeholder="Search" className="px-4 py-2 border rounded-lg" />
                         <button className="px-4 py-2 border rounded-lg">Filters</button>
                     </div>
-                    <button
-                        className="bg-blue-500 text-white font-bold py-2 px-4 rounded flex items-center gap-4"
-                        onClick={handleCreateUser}
-                    >
-                        <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4" />
-                        </svg>
-                        Create User
-                    </button>
+                    <div className='flex items-center gap-4'>
+                        <button className="p-2 rounded-lg hover:bg-gray-100">
+                            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" />
+                            </svg>
+                        </button>
+                        <button className="p-2 rounded-lg hover:bg-gray-100">
+                            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+                            </svg>
+                        </button>
+                        <button
+                            className="bg-blue-500 text-white font-bold py-2 px-4 rounded flex items-center gap-4"
+                            onClick={handleCreateUser}
+                        >
+                            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4" />
+                            </svg>
+                            Create User
+                        </button>
+                    </div>
                 </div>
                 {/* Make the table responsive */}
                 <div className="overflow-x-auto">
