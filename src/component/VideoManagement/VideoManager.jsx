c7ac4ce4ebe30cc01a4f06bab7a94bc6c887ecff
Tran Chi Cong <tranchicong.dev@gmail.com>
2024-11-07 21:20:37 +0700

    Modified Load videos by BE data

--- src/component/VideoManagement/VideoManager.jsx
+++ src/component/VideoManagement/VideoManager.jsx
@@ -6,27 +6,45 @@ import axios from 'axios';
 
 const VideoManager = () => {
     const [videos, setVideos] = useState([]);
-    const [itemsPerPage, setItemsPerPage] = useState(12);
+    const [itemsPerPage, setItemsPerPage] = useState(10);
     const [page, setPage] = useState(1);
     const totalPages = Math.ceil(videos.length / itemsPerPage);
     const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
     const [playingVideo, setPlayingVideo] = useState(null);
     const [showForm, setShowForm] = useState(false);
     const [editingVideo, setEditingVideo] = useState(null); 
+    const [size, setSize] = useState(10);
+    const [sortBy, setSortBy] = useState('date');
+    const [order, setOrder] = useState('descending');
+    const [title, setTitle] = useState('');
+    const [enumMode, setEnumMode] = useState('');
     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzFhMTRmNDZkMDUwODg0MjNlZWFiOTEiLCJpcCI6Ijo6MSIsImlhdCI6MTczMDQ2MDgxN30._dqyZS4blv-60Ii18LOfGNzkutur_fXJy80H1NKJyRE';
     useEffect(() => {
-        axios.get('https://social-media-z5a2.onrender.com/api/videos/', {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
-            .then(response => {
+        console.log(title,
+            enumMode,
+            sortBy,
+            order,
+            size,
+            page,);
+        
+        const fetchVideos = async () => {
+            try {
+                const response = await axios.get('https://social-media-z5a2.onrender.com/api/videos/', {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                    params: {
+                        title, enumMode,sortBy, order,size,page,       
+                    },
+                });
                 setVideos(response.data.videos);
-            })
-            .catch(error => {
+            } catch (error) {
                 console.error('Error fetching data:', error);
-            });
-    }, []);
+            }
+        };
+
+        fetchVideos();
+    }, [title, enumMode, sortBy, order, size, page, token]);
 
     const deleteVideo = (id) => {
         if (window.confirm("Bạn có chắc chắn muốn xóa video này không?")) {
@@ -54,21 +72,17 @@ const VideoManager = () => {
     };
   
     const editVideo = async (updatedVideo) => {
+        console.log(updatedVideo);
         const formData = new FormData();
         formData.append('title', updatedVideo.title);
         formData.append('description', updatedVideo.description);
-        updatedVideo.categoryIds.forEach((id, index) => {
-            formData.append(`categoryIds[${index}]`, id);
-        });
+        formData.append('categoryIds', '671a01672a386fca99c73c02');
+
+
         formData.append('enumMode', updatedVideo.enumMode);
-        if (typeof updatedVideo.thumbnailUrl === 'string' && updatedVideo.thumbnailUrl) {
-            const response = await fetch(updatedVideo.thumbnailUrl, { mode: 'cors' });
-            const blob = await response.blob();
-            formData.append('videoThumbnail', blob, 'thumbnail.jpg');
-        } else if (updatedVideo.thumbnailUrl instanceof File) {
+         if (updatedVideo.thumbnailUrl instanceof File) {
             formData.append('videoThumbnail', updatedVideo.thumbnailUrl);
         }
-
         axios.patch(`https://social-media-z5a2.onrender.com/api/videos/${updatedVideo.id}`, formData, {
             headers: {
                 'Authorization': `Bearer ${token}`,
@@ -77,7 +91,6 @@ const VideoManager = () => {
         })
             .then(response => {
                 console.log('Video updated:', response.data);
-                window.location.reload(); 
             })
             .catch(error => {
                 console.error('Error updating video:', error);
@@ -88,15 +101,72 @@ const VideoManager = () => {
 
     return (
         <div className="container mx-auto flex flex-col gap-4 bg-white p-8">
-            <div className="flex justify-between items-center mb-6">
-                <div className="flex items-center space-x-4">
+            <div className="grid grid-cols-3 gap-4">
+                <div>
+                    <label>Size:</label>
                     <input
-                        type="text"
-                        placeholder="Search"
-                        className="px-4 py-2 border rounded-lg"
+                        type="number"
+                        min="1"
+                        value={size}
+                        onChange={(e) => setSize(Number(e.target.value))}
+                        className="border rounded px-2 py-1 w-full"
                     />
-                    <button className="px-4 py-2 border rounded-lg">Filters</button>
                 </div>
+                <div>
+                    <label>Sort By:</label>
+                    <select
+                        value={sortBy}
+                        onChange={(e) => setSortBy(e.target.value)}
+                        className="border rounded px-2 py-1 w-full"
+                    >
+                        <option value="">--</option>
+                        <option value="date">Date</option>
+                        <option value="likes">Likes</option>
+                        <option value="views">Views</option>
+                    </select>
+                </div>
+                <div>
+                    <label>Order:</label>
+                    <select
+                        value={order}
+                        onChange={(e) => setOrder(e.target.value)}
+                        className="border rounded px-2 py-1 w-full"
+                    >
+                        <option value="">--</option>
+                        <option value="ascending">Ascending</option>
+                        <option value="descending">Descending</option>
+                    </select>
+                </div>
+                <div>
+                    <label>Title:</label>
+                    
+                    <div className="flex items-center ">
+                        <input
+                            type="text"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            className="border rounded px-2 py-1 w-full"
+                        />
+                        <button className="px-2 py-1 border rounded-lg">Filters</button>
+                    </div>
+                </div>
+                <div>
+                    <label>Type:</label>
+                    <select
+                        value={enumMode}
+                        onChange={(e) => setEnumMode(e.target.value)}
+                        className="border rounded px-2 py-1 w-full"
+                    >
+                        <option value="">--</option>
+                        <option value="public">Public</option>
+                        <option value="private">Private</option>
+                        <option value="unlisted">Unlisted</option>
+                        <option value="draft">Draft</option>
+                        <option value="member">Member</option>
+                    </select>
+
+                </div>
+                
                 <div className="flex items-center space-x-2">
                     <button className="p-2 rounded-lg hover:bg-gray-100">
                         <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -110,6 +180,8 @@ const VideoManager = () => {
                     </button>
                 </div>
             </div>
+
+                
 
             <div className="overflow-x-auto">
                 <table className="min-w-full bg-white">
@@ -168,16 +240,22 @@ const VideoManager = () => {
                         >
                             Previous
                         </button>
-                        {pages.map((p) => (
-                            <button
-                                key={p}
-                                onClick={() => setPage(p)}
-                                className={`px-3 py-1 rounded-lg ${p === page ? 'bg-blue-600 text-white' : 'border hover:bg-gray-50'
-                                    }`}
-                            >
-                                {p}
-                            </button>
-                        ))}
+                        {pages
+                            .filter(p => Math.abs(p - page) <= 2 || p === 1 || p === totalPages) // Hiển thị trang đầu, cuối và các trang gần trang hiện tại
+                            .map((p, index, array) => (
+                                <>
+                                    {index > 0 && p - array[index - 1] > 1 && <span key={`ellipsis-${p}`} className="px-2">...</span>}
+                                    <button
+                                        key={p}
+                                        onClick={() => setPage(p)}
+                                        className={`px-3 py-1 rounded-lg ${p === page ? 'bg-blue-600 text-white' : 'border hover:bg-gray-50'}`}
+                                    >
+                                        {p}
+                                    </button>
+                                </>
+                            ))
+                        }
+
                         <button
                             disabled={page === totalPages}
                             onClick={() => setPage(page + 1)}
